2de2e8d10c04149d57094b1c0df7c6f7af242e11
AzerKhamassi <azer.khamassi@gmail.com>
2020-04-07 20:35:26 +0100

    first commit

--- src/app/people/people-list/people-list.component.ts
+++ src/app/people/people-list/people-list.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { PeopleService } from '../people.service';
-import { People } from '../people';
+import { Component, OnInit, Input } from "@angular/core";
+import { PeopleService } from "../people.service";
+import { People } from "../people";
 
 @Component({
-  selector: 'app-people-list',
-  templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.css']
+  selector: "app-people-list",
+  templateUrl: "./people-list.component.html",
+  styleUrls: ["./people-list.component.css"],
 })
-export class PeopleListComponent implements OnInit{
-  searchText
-  people: People [] = [];
-  constructor(private peopleService:PeopleService) { }
+export class PeopleListComponent implements OnInit {
+  searchText;
+  people: People[] = [];
+  constructor(private peopleService: PeopleService) {}
 
   ngOnInit(): void {
     this.people = this.peopleService.getPeople();
   }
-  onDelete(id: number){
+  onDelete(id: number) {
     this.peopleService.deletePeople(id);
   }
 }
